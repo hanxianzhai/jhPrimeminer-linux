0f21882c57d801d39a789df425622f14f5358cfc
Tricky.HP <tricky@t4v.net>
2013-10-25 19:38:27 +0700

    Modify Multi PrimorialMultiplier per Thread

---
+++ src/primecoinMiner/miner.cpp
@@ -0,0 +1,181 @@
+#include"global.h"
+#include <ctime>
+#include "ticker.h"
+
+
+bool MineProbablePrimeChain(CSieveOfEratosthenes*& psieve, primecoinBlock_t* block, mpz_class& bnFixedMultiplier, bool& fNewBlock, unsigned int& nTriedMultiplier, unsigned int& nProbableChainLength, 
+							unsigned int& , unsigned int& nPrimesHit, sint32 threadIndex, mpz_class& mpzHash, unsigned int nPrimorialMultiplier);
+
+std::set<mpz_class> multiplierSet;
+
+void BitcoinMiner(primecoinBlock_t* primecoinBlock, CSieveOfEratosthenes*& psieve, sint32 threadIndex)
+{
+	//printf("PrimecoinMiner started\n");
+	//SetThreadPriority(THREAD_PRIORITY_LOWEST);
+	//RenameThread("primecoin-miner");
+	if( pctx == NULL )
+		pctx = BN_CTX_new();
+	// Each thread has its own key and counter
+	//CReserveKey reservekey(pwallet);
+//	unsigned int nExtraNonce = 0;  unused?
+
+	static const unsigned int nPrimorialHashFactor = 7;
+//	const unsigned int nPrimorialMultiplierStart = 61;   unused?
+//	const unsigned int nPrimorialMultiplierMax = 79;  unused?
+    unsigned int nPrimorialMultiplier = primeStats.nPrimorialMultiplier;
+    switch(threadIndex) {
+        case 1:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier2;
+            break;
+        case 2:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier3;
+            break;
+        case 3:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier4;
+            break;
+        case 5:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier2;
+            break;
+        case 6:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier3;
+            break;
+        case 7:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier4;
+            break;
+        default:
+            nPrimorialMultiplier = primeStats.nPrimorialMultiplier;
+            break;
+    }
+//	uint64_t nTimeExpected = 0;   // time expected to prime chain (micro-second)   unused?
+//	uint64_t nTimeExpectedPrev = 0; // time expected to prime chain last time   unused?
+//	bool fIncrementPrimorial = true; // increase or decrease primorial factor   unused?
+//	uint64_t nSieveGenTime = 0;   unused?
+	
+
+	//primecoinBlock->nonce = 0;
+	//TODO: check this if it makes sense
+	primecoinBlock->nonce = 0x01000000 * threadIndex;
+   const unsigned long maxNonce = (0x01000000 * threadIndex) | 0x00FF0000;
+	//primecoinBlock->nonce = 0;
+
+	uint64 nTime = getTimeMilliseconds() + 1000*600;
+//	uint64 nStatTime = getTimeMilliseconds() + 2000;  unused?
+	
+	// note: originally a wanted to loop as long as (primecoinBlock->workDataHash != jhMiner_getCurrentWorkHash()) did not happen
+	//		 but I noticed it might be smarter to just check if the blockHeight has changed, since that is what is really important
+	uint32 loopCount = 0;
+
+	//mpz_class mpzHashFactor;
+	//Primorial(nPrimorialHashFactor, mpzHashFactor);
+	unsigned int nHashFactor = PrimorialFast(nPrimorialHashFactor);
+
+	time_t unixTimeStart;
+	time(&unixTimeStart);
+	uint32 nTimeRollStart = primecoinBlock->timestamp - 5;
+   uint32 nLastRollTime = getTimeMilliseconds();
+	uint32 nCurrentTick = nLastRollTime;
+	while( nCurrentTick < nTime && primecoinBlock->serverData.blockHeight == jhMiner_getCurrentWorkBlockHeight(primecoinBlock->threadIndex) )
+			{
+		nCurrentTick = getTimeMilliseconds();
+      // Roll Time stamp every 10 secs.
+		if ((primecoinBlock->xptMode) && (nCurrentTick < nLastRollTime || (nLastRollTime - nCurrentTick >= 10000)))
+		{
+			// when using x.pushthrough, roll time
+			time_t unixTimeCurrent;
+			time(&unixTimeCurrent);
+			uint32 timeDif = unixTimeCurrent - unixTimeStart;
+			uint32 newTimestamp = nTimeRollStart + timeDif;
+			if( newTimestamp != primecoinBlock->timestamp )
+			{
+				primecoinBlock->timestamp = newTimestamp;
+         	primecoinBlock->nonce = 0x01000000 * threadIndex;
+		//		primecoinBlock->nonce = 0;
+				//nPrimorialMultiplierStart = startFactorList[(threadIndex&3)];
+		//		nPrimorialMultiplier = nPrimorialMultiplierStart;
+			}
+         nLastRollTime = nCurrentTick;
+		}
+
+		primecoinBlock_generateHeaderHash(primecoinBlock, primecoinBlock->blockHeaderHash.begin());
+		//
+		// Search
+		//
+		bool fNewBlock = true;
+		unsigned int nTriedMultiplier = 0;
+		// Primecoin: try to find hash divisible by primorial
+        uint256 phash = primecoinBlock->blockHeaderHash;
+        mpz_class mpzHash;
+        mpz_set_uint256(mpzHash.get_mpz_t(), phash);
+        
+		while ((phash < hashBlockHeaderLimit || !mpz_divisible_ui_p(mpzHash.get_mpz_t(), nHashFactor)) && primecoinBlock->nonce < maxNonce) {
+			primecoinBlock->nonce++;
+			primecoinBlock_generateHeaderHash(primecoinBlock, primecoinBlock->blockHeaderHash.begin());
+            phash = primecoinBlock->blockHeaderHash;
+            mpz_set_uint256(mpzHash.get_mpz_t(), phash);
+		}
+		//printf("Use nonce %d\n", primecoinBlock->nonce);
+		if (primecoinBlock->nonce >= maxNonce)
+		{
+			printf("Nonce overflow\n");
+			break;
+		}
+		// Primecoin: primorial fixed multiplier
+		mpz_class mpzPrimorial;
+		unsigned int nRoundTests = 0;
+		unsigned int nRoundPrimesHit = 0;
+//		uint64 nPrimeTimerStart = getTimeMilliseconds();   unused?
+		
+		//if( loopCount > 0 )
+		//{
+		//	//primecoinBlock->nonce++;
+		//	if (!PrimeTableGetNextPrime(nPrimorialMultiplier))
+		//		error("PrimecoinMiner() : primorial increment overflow");
+		//}
+
+		Primorial(nPrimorialMultiplier, mpzPrimorial);
+
+		unsigned int nTests = 0;
+		unsigned int nPrimesHit = 0;
+		
+		mpz_class mpzMultiplierMin = mpzPrimeMin * nHashFactor / mpzHash + 1;
+		while (mpzPrimorial < mpzMultiplierMin )
+		{
+			if (!PrimeTableGetNextPrime(nPrimorialMultiplier))
+				error("PrimecoinMiner() : primorial minimum overflow");
+			Primorial(nPrimorialMultiplier, mpzPrimorial);
+		}
+        mpz_class mpzFixedMultiplier;
+        if (mpzPrimorial > nHashFactor) {
+            mpzFixedMultiplier = mpzPrimorial / nHashFactor;
+        } else {
+            mpzFixedMultiplier = 1;
+        }		
+		//printf("fixedMultiplier: %d nPrimorialMultiplier: %d\n", BN_get_word(&bnFixedMultiplier), nPrimorialMultiplier);
+		// Primecoin: mine for prime chain
+		unsigned int nProbableChainLength;
+        //std::cout << "DEBUG: Th#:"<< threadIndex << "-Prim: "<<nPrimorialMultiplier << std::endl;
+        MineProbablePrimeChain(psieve, primecoinBlock, mpzFixedMultiplier, fNewBlock, nTriedMultiplier, nProbableChainLength, nTests, nPrimesHit, threadIndex, mpzHash, nPrimorialMultiplier);
+
+		//{
+		//	// do nothing here, share is already submitted in MineProbablePrimeChain()
+		//	//primecoinBlock->nonce += 0x00010000;
+		//	primecoinBlock->nonce++;
+		//	nPrimorialMultiplier = primeStats.nPrimorialMultiplier;
+		//	//break;
+		//}
+		//psieve = NULL;
+		nRoundTests += nTests;
+		nRoundPrimesHit += nPrimesHit;
+		//nPrimorialMultiplier = primeStats.nPrimorialMultiplier;
+		// added this
+		//if (fNewBlock)
+		//{
+		//}
+
+
+		primecoinBlock->nonce ++;
+		//primecoinBlock->timestamp = max(primecoinBlock->timestamp, (unsigned int) time(NULL));
+		loopCount++;
+	}
+	
+}
---
+++ src/primecoinMiner/xptClientPacketHandler.cpp
@@ -0,0 +1,125 @@
+#include"global.h"
+
+/*
+ * Called when a packet with the opcode XPT_OPC_S_AUTH_ACK is received
+ */
+bool xptClient_processPacket_authResponse(xptClient_t* xptClient)
+{
+	xptPacketbuffer_t* cpb = xptClient->recvBuffer;
+	// read data from the packet
+	xptPacketbuffer_beginReadPacket(cpb);
+	// start parsing
+	bool readError = false;
+	// read error code field
+	uint32 authErrorCode = xptPacketbuffer_readU32(cpb, &readError);
+	if( readError )
+		return false;
+	// read reject reason / motd
+	char rejectReason[512];
+	xptPacketbuffer_readString(cpb, rejectReason, 512, &readError);
+	rejectReason[511] = '\0';
+	if( readError )
+		return false;
+	if( authErrorCode == 0 )
+	{
+		xptClient->clientState = XPT_CLIENT_STATE_LOGGED_IN;
+			std::cout << "xpt: Logged in" << std::endl;
+			if( rejectReason[0] != '\0' )
+				std::cout << "Message from server: " << rejectReason << std::endl;
+	}
+	else
+	{
+		// error logging in -> disconnect
+			std::cout << "xpt: Failed to log in" << std::endl;
+		if( rejectReason[0] != '\0' )
+			std::cout << "Reason: " << rejectReason << std::endl;
+		return false;
+	}
+	return true;
+}
+
+/*
+ * Called when a packet with the opcode XPT_OPC_S_WORKDATA1 is received
+ * This is the first version of xpt 'getwork'
+ */
+bool xptClient_processPacket_blockData1(xptClient_t* xptClient)
+{
+	// parse block data
+	bool recvError = false;
+	xptPacketbuffer_beginReadPacket(xptClient->recvBuffer);
+	xptClient->workDataValid = false;
+	// add general block info
+	xptClient->blockWorkInfo.version = xptPacketbuffer_readU32(xptClient->recvBuffer, &recvError);			// version
+	xptClient->blockWorkInfo.height = xptPacketbuffer_readU32(xptClient->recvBuffer, &recvError);			// block height
+	xptClient->blockWorkInfo.nBits = xptPacketbuffer_readU32(xptClient->recvBuffer, &recvError);			// nBits
+	xptClient->blockWorkInfo.nBitsShare = xptPacketbuffer_readU32(xptClient->recvBuffer, &recvError);		// nBitsRecommended / nBitsShare
+	xptClient->blockWorkInfo.nTime = xptPacketbuffer_readU32(xptClient->recvBuffer, &recvError);			// nTimestamp
+	xptPacketbuffer_readData(xptClient->recvBuffer, xptClient->blockWorkInfo.prevBlock, 32, &recvError);	// prevBlockHash
+	uint32 payloadNum = xptPacketbuffer_readU32(xptClient->recvBuffer, &recvError);							// payload num
+	if( recvError )
+	{
+			std::cout << "xptClient_processPacket_blockData1(): Parse error" << std::endl;
+		return false;
+	}
+	if( xptClient->payloadNum != payloadNum )
+	{
+			std::cout << "xptClient_processPacket_blockData1(): Invalid payloadNum" << std::endl;
+		return false;
+	}
+	for(uint32 i=0; i<payloadNum; i++)
+	{
+		// read merkle root for each work data entry
+		xptPacketbuffer_readData(xptClient->recvBuffer, xptClient->workData[i].merkleRoot, 32, &recvError);
+	}
+	if( recvError )
+	{
+			std::cout << "xptClient_processPacket_blockData1(): Parse error 2" << std::endl;
+		return false;
+	}
+	xptClient->workDataValid = true;
+	xptClient->workDataCounter++;
+	return true;
+}
+
+/*
+ * Called when a packet with the opcode XPT_OPC_S_SHARE_ACK is received
+ */
+bool xptClient_processPacket_shareAck(xptClient_t* xptClient)
+{
+	xptPacketbuffer_t* cpb = xptClient->recvBuffer;
+	// read data from the packet
+	xptPacketbuffer_beginReadPacket(cpb);
+	// start parsing
+	bool readError = false;
+	// read error code field
+	uint32 shareErrorCode = xptPacketbuffer_readU32(cpb, &readError);
+	if( readError )
+		return false;
+	// read reject reason
+	char rejectReason[512];
+	xptPacketbuffer_readString(cpb, rejectReason, 512, &readError);
+	rejectReason[511] = '\0';
+	float shareValue = xptPacketbuffer_readFloat(cpb, &readError);
+	if( readError )
+		return false;
+	if( shareErrorCode == 0 )
+	{
+		total_shares++;
+		valid_shares++;
+		time_t now = time(0);
+		char* dt = ctime(&now);
+			//std::cout << "ACCEPTED [ " << valid_shares << " / " << total_shares << " val: " << shareValue << "] " << dt << std::endl;
+		primeStats.fShareValue += shareValue;
+		primeStats.fBlockShareValue += shareValue;
+		primeStats.fTotalSubmittedShareValue += shareValue;
+	}
+	else
+	{
+		// error logging in -> disconnect
+		total_shares++;
+			//std::cout << "Invalid share" << std::endl;
+			//if( rejectReason[0] != '\0' )
+			//	std::cout << "Reason: " << rejectReason << std::endl;
+	}
+	return true;
+}
